8c49b8e6822b5d754027ee9ad6b00b882a63ba1e
Nuno Fidalgo <nunorfidalgo@gmail.com>
2018-07-02 16:53:11 +0100

    Add project files.

---
+++ RandomNumberGenerator/MyForm.h
@@ -0,0 +1,240 @@
+#pragma once
+#include <iostream>
+#include <cstdlib>
+#include <time.h>
+
+namespace RandomNumberGenerator {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Summary for MyForm
+	/// </summary>
+	public ref class MyForm : public System::Windows::Forms::Form
+	{
+	public:
+		MyForm(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: Add the constructor code here
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		~MyForm()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::Label^  label1;
+	protected:
+	private: System::Windows::Forms::Label^  label2;
+	private: System::Windows::Forms::Label^  label3;
+	private: System::Windows::Forms::Label^  label4;
+	private: System::Windows::Forms::Label^  label5;
+	private: System::Windows::Forms::Label^  label6;
+	private: System::Windows::Forms::Label^  label7;
+	private: System::Windows::Forms::Button^  gen_btn;
+	private: System::Windows::Forms::Button^  reset_btn;
+
+
+
+	private: System::Windows::Forms::PictureBox^  pictureBox1;
+
+	private:
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			System::ComponentModel::ComponentResourceManager^  resources = (gcnew System::ComponentModel::ComponentResourceManager(MyForm::typeid));
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->label4 = (gcnew System::Windows::Forms::Label());
+			this->label5 = (gcnew System::Windows::Forms::Label());
+			this->label6 = (gcnew System::Windows::Forms::Label());
+			this->label7 = (gcnew System::Windows::Forms::Label());
+			this->gen_btn = (gcnew System::Windows::Forms::Button());
+			this->reset_btn = (gcnew System::Windows::Forms::Button());
+			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
+			this->SuspendLayout();
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(140, 24);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(13, 13);
+			this->label1->TabIndex = 0;
+			this->label1->Text = L"0";
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Location = System::Drawing::Point(199, 24);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(13, 13);
+			this->label2->TabIndex = 1;
+			this->label2->Text = L"0";
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->Location = System::Drawing::Point(262, 24);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(13, 13);
+			this->label3->TabIndex = 2;
+			this->label3->Text = L"0";
+			// 
+			// label4
+			// 
+			this->label4->AutoSize = true;
+			this->label4->Location = System::Drawing::Point(317, 24);
+			this->label4->Name = L"label4";
+			this->label4->Size = System::Drawing::Size(13, 13);
+			this->label4->TabIndex = 3;
+			this->label4->Text = L"0";
+			// 
+			// label5
+			// 
+			this->label5->AutoSize = true;
+			this->label5->Location = System::Drawing::Point(374, 24);
+			this->label5->Name = L"label5";
+			this->label5->Size = System::Drawing::Size(13, 13);
+			this->label5->TabIndex = 4;
+			this->label5->Text = L"0";
+			// 
+			// label6
+			// 
+			this->label6->AutoSize = true;
+			this->label6->Location = System::Drawing::Point(432, 24);
+			this->label6->Name = L"label6";
+			this->label6->Size = System::Drawing::Size(13, 13);
+			this->label6->TabIndex = 5;
+			this->label6->Text = L"0";
+			// 
+			// label7
+			// 
+			this->label7->AutoSize = true;
+			this->label7->Location = System::Drawing::Point(497, 24);
+			this->label7->Name = L"label7";
+			this->label7->Size = System::Drawing::Size(13, 13);
+			this->label7->TabIndex = 6;
+			this->label7->Text = L"0";
+			// 
+			// gen_btn
+			// 
+			this->gen_btn->Location = System::Drawing::Point(343, 83);
+			this->gen_btn->Name = L"gen_btn";
+			this->gen_btn->Size = System::Drawing::Size(75, 23);
+			this->gen_btn->TabIndex = 7;
+			this->gen_btn->Text = L"Generate";
+			this->gen_btn->UseVisualStyleBackColor = true;
+			this->gen_btn->Click += gcnew System::EventHandler(this, &MyForm::gen_btn_Click);
+			// 
+			// reset_btn
+			// 
+			this->reset_btn->Location = System::Drawing::Point(435, 83);
+			this->reset_btn->Name = L"reset_btn";
+			this->reset_btn->Size = System::Drawing::Size(75, 23);
+			this->reset_btn->TabIndex = 8;
+			this->reset_btn->Text = L"Reset";
+			this->reset_btn->UseVisualStyleBackColor = true;
+			this->reset_btn->Click += gcnew System::EventHandler(this, &MyForm::reset_btn_Click);
+			// 
+			// pictureBox1
+			// 
+			this->pictureBox1->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox1.Image")));
+			this->pictureBox1->Location = System::Drawing::Point(12, 24);
+			this->pictureBox1->Name = L"pictureBox1";
+			this->pictureBox1->Size = System::Drawing::Size(101, 82);
+			this->pictureBox1->SizeMode = System::Windows::Forms::PictureBoxSizeMode::StretchImage;
+			this->pictureBox1->TabIndex = 9;
+			this->pictureBox1->TabStop = false;
+			this->pictureBox1->Click += gcnew System::EventHandler(this, &MyForm::pictureBox1_Click);
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(549, 121);
+			this->Controls->Add(this->pictureBox1);
+			this->Controls->Add(this->reset_btn);
+			this->Controls->Add(this->gen_btn);
+			this->Controls->Add(this->label7);
+			this->Controls->Add(this->label6);
+			this->Controls->Add(this->label5);
+			this->Controls->Add(this->label4);
+			this->Controls->Add(this->label3);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->label1);
+			this->Name = L"MyForm";
+			this->Text = L"Number Generator";
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->EndInit();
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
+		}
+#pragma endregion
+	private: System::Void pictureBox1_Click(System::Object^  sender, System::EventArgs^  e) {
+	}
+	private: System::Void gen_btn_Click(System::Object^  sender, System::EventArgs^  e) {
+
+		int num[7] = { 0 };
+
+		// seed
+		srand((int)time(0));
+
+		// Randomize the array values.
+		for (int i = 0; i < 7; i++)
+			num[i] = (rand() % 99) + 1;
+
+		// set the label text with random number
+		this->label1->Text = Convert::ToString(num[0]);
+		this->label2->Text = Convert::ToString(num[1]);
+		this->label3->Text = Convert::ToString(num[2]);
+		this->label4->Text = Convert::ToString(num[3]);
+		this->label5->Text = Convert::ToString(num[4]);
+		this->label6->Text = Convert::ToString(num[5]);
+		this->label7->Text = Convert::ToString(num[6]);
+
+		// change the button states.
+		this->gen_btn->Enabled = false;
+		this->reset_btn->Enabled = true;
+	}
+	private: System::Void reset_btn_Click(System::Object^  sender, System::EventArgs^  e) {
+		// clear label fields
+		this->label1->Text = "0";
+		this->label2->Text = "0";
+		this->label3->Text = "0";
+		this->label4->Text = "0";
+		this->label5->Text = "0";
+		this->label6->Text = "0";
+		this->label7->Text = "0";
+
+		// set button state
+		this->gen_btn->Enabled = true;
+		this->reset_btn->Enabled = false;
+	}
+	};
+}
